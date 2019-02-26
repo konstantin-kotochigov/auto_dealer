3b29235fc0d69ce5a3b90d133a1179d7cfb69532
KK <konstantin.kotochigov@gmail.com>
2019-02-26 22:29:18 +0300

    train dataset

--- cj.py
+++ cj.py
@@ -12,7 +12,7 @@ spark.udf.register("cj_id", cj_id, ArrayType(StringType()))
 def cj_attr(cj_attributes, arg_id, arg_key=None):
     result = []
     if cj_attributes is not None:
-        for attr in cj_attributes:
+        for attr in cj_attributes:1
             for member_id in range(0, 8):
                 member_name = 'member' + str(member_id)
                 if attr is not None and member_name in attr:
@@ -45,65 +45,46 @@ cj_df.groupby("crmid").count().show()
 
 
 
+# Get ID, link and TS
 cj_df = spark.sql('''
 select
     cj_id(id, 10008, 10031)[0] as fpc,
-    cj_id(id, 10008, 10032)[0] as userid,
-    date(from_unixtime(ts/1000)) as ts
+    substring(substring_index(substring_index(cj_attr(attributes, 10071)[0], '?', 1), '#', 1), 19, 100) as link,
+    ts/1000 as ts
 from cj c
-''').filter("fpc is null and userid is null")
+''').filter("fpc is not null")
 
+# Compute TS deltas between events (in hours)
 cj_df.createOrReplaceTempView("cj_df")
-cj_df = spark.sql("select fpc,  count(distinct ts) as cnt from cj_df group by fpc")
-cj_df.createOrReplaceTempView("cj_df")
+cj_df_attrs = spark.sql("select fpc, link, ts, lead(ts) over (partition by fpc order by ts) as next_ts from cj_df")
+cj_df_attrs = cj_df_attrs.withColumn("next",(cj_df_attrs["next_ts"]-cj_df_attrs["ts"]) / 3600)
+cj_df_attrs.createOrReplaceTempView("cj_df_attrs")
 
-userid_cnt = cj_df.select("userid").distinct().count()
-event_stat = cj_df.groupby("userid").count().toDF("userid","cnt").groupby("cnt").count().orderBy("cnt")
+# Assign surrogate ID to FPCs
+fpc_dict = spark.sql("select fpc, rank() over (order by fpc) as rnk from (select distinct fpc from cj_df)")
+fpc_dict.createOrReplaceTempView("fpc_dict")
+cj_df = spark.sql("select t1.link, t1.next, t2.rnk as id, t1.ts from cj_df_attrs t1 join fpc_dict t2 on t1.fpc = t2.fpc order by t1.ts").select("id","ts","next","link")
+
+# Create Records
+def groupAttrs(r):
+    sortedList = sorted(r[1], key=lambda y: y[0])
+    dividedList = list(zip(*sortedList))
+    # dt = [x[0] for x in sortedList]
+    deltas = [i for i, x in enumerate(dividedList[1]) if x == None or x > 4]
+    return [(r[0], dividedList[1][0:y+1], dividedList[2][0:y+1], 1 if dividedList[1][y]==None else 0) for y in deltas]
+
+# Group all urls by ID
+y = cj_df.rdd.map(lambda x: (x['id'], (x['ts'], x['next'], x['link']))).groupByKey().flatMap(lambda x: groupAttrs(x))
+
+# Convert ot Python
+y_py = pandas.DataFrame(y.collect(),  columns=['id','dt','url','target'])
+y_py.head()
 
 
-cj_features_raw = spark.sql('''select cj_id(id, 10008, 10032)[0] as crmid, date(from_unixtime(ts/1000)) as ts from cj''').filter("crmid is not null and crmid!='undefined'").groupby("crmid").count()
-# cj_features_raw = spark.sql("select cj_id(id, 10008, 10032) as ts from cj")
-# spark_df = spark.createDataFrame(df)_
+cj_df['target'] = 1
 
-cj_features_raw_py = cj_features_raw.collect()
-cj_ids = pandas.DataFrame([(float(x[0]),x[1]) for x in cj_features_raw_py], columns=['crmid','ts'])
-
-available_train_py = train_py[train_py.app_ts >= datetime.date(2018,12, 25)][["crmid","app_ts"]]
-available_scoring_py = train_py[train_py.app_ts >= datetime.date(2018,12, 25)][["crmid","app_ts"]]
+def divide_into_sessions(r):
+    return ([])
 
 
-cj_df = cj_ids.join(df[['crmid','app_ts']].set_index("crmid"), on="crmid", how='inner').groupby("crmid", as_index=False).count()[["crmid","ts"]]
-# cj_df_scoring = cj_ids.join(scoring_py[['crmid','app_ts']].set_index("crmid"), on="crmid", how='inner').groupby("crmid", as_index=False).count()[["crmid","ts"]]
-df = df.join(cj_df.set_index("crmid"), on="crmid", how="left")
-# scoring_py = scoring_py.join(cj_df_scoring.set_index("crmid"), on="crmid", how="left")
-df["ts"] = df.ts.fillna(0)
-# scoring_py["ts"] = scoring_py.ts.fillna(0)
 
-numeric_attrs.append("ts")
-
-# cj_df.createOrReplaceTempView("cj_df")
-
-base.fillna(app_ts => current_dt)
-base = base.filter(app_ts > ts and ts > app_ts - 2W)
-
-features = spark.sql("""
-    select
-        crmid,
-        app_ts,
-        sum(case when device_type==10000 then 1 else 0 end) as f1_cnt,
-        sum(case when device_type==10001 then 1 else 0 end) as f2_cnt,
-        sum(case when device_type==10002 then 1 else 0 end) as f3_cnt,
-        sum(case when device_type==10003 then 1 else 0 end) as f4_cnt,
-        max(case when device_type==10000 then ts else to_date('1900-01-01') end) as f1_prev,
-        max(case when device_type==10001 then ts else to_date('1900-01-01') end) as f2_prev,
-        max(case when device_type==10002 then ts else to_date('1900-01-01') end) as f3_prev,
-        max(case when device_type==10003 then ts else to_date('1900-01-01') end) as f4_prev,
-        count(*) as cnt
-    from
-        cj_df
-    group by
-        crmid,
-        app_ts
-""")
-
-features.coalesce(1).write.parquet("")
--- load.py
+++ load.py
@@ -14,8 +14,8 @@ cp_path = "/data/57efd33d-aaa5-409d-89ce-ff29a86d78a5/.dmpkit/profiles/master/cd
 
 # Load Customer Journey
 cj_all = spark.read.format("com.databricks.spark.avro").load(cj_path)
-time_from = int(time.mktime(datetime.datetime(2017, 12, 20).timetuple())) * 1000
-time_to = int(time.mktime(datetime.datetime(2019, 12, 30).timetuple())) * 1000
+time_from = int(time.mktime(datetime.datetime(2018, 12, 20).timetuple())) * 1000
+time_to = int(time.mktime(datetime.datetime(2018, 12, 30).timetuple())) * 1000
 cj = cj_all.filter('ts > {} and ts < {}'.format(time_from, time_to))
 cj.createOrReplaceTempView('cj')
 
