094cff9b19bc7d87696d737ae4f9047f17cb8cfc
kostantin-kotochigov <konstantin.kotochigov@gmail.com>
2019-04-11 17:53:33 +0300

    command

--- analytic_attributes.sh
+++ analytic_attributes.sh
@@ -7,14 +7,20 @@ PYSPARK_PYTHON=./venv/bin/python spark2-submit --master yarn --deploy-mode clust
  --conf "spark.pyspark.python=./venv/bin/python" \
  --jars "spark-avro_2.11-3.2.0.jar" \
  --archives venv.zip#venv \
- --conf "spark.executor.memory=10g" \
+ --conf "spark.executor.memory=2g" \
+ --conf "spark.dynamicAllocation.enabled=True" \
+ --conf "spark.dynamicAllocation.minExecutors=12" \
  --conf "spark.driver.memory=20g" \
+ --conf "spark.shuffle.service.enabled=True" \
  --conf "spark.yarn.executor.memoryOverhead=10g" \
  --conf "spark.kryoserializer.buffer.max=2047m" \
  --conf "spark.driver.maxResultSize=10g" \
  --conf "spark.rpc.maxSize=1g" \
- --conf "spark.driver.cores=4" \
+ --conf "spark.driver.cores=3" \
+ --conf "spark.eventLog.enabled=true" \
+ --conf "spark.eventLog.dir=hdfs://nameservice1/user/spark/spark2ApplicationHistory"  \
+ --conf "spark.yarn.historyServer.address=http://bmw-prod-mn2:18089" \
  --files schema.avsc \
  --py-files "cj_loader.py,cj_predictor.py,cj_export.py" \
  --name "analytic_attributes" \
- main.py nosend refit_auto 0.25
+ main.py send refit_auto 0.25
